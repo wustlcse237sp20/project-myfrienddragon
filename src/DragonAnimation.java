0175e71be8c092301be865af2102bec84e31a43e
smithj11 <julia.smith@wustl.edu>
2020-03-30 12:09:43 -0400

    Dragon Animation Class and Std Draw dependency added

---
+++ src/DragonAnimation.java
@@ -0,0 +1,102 @@
+
+
+//class puts resources into animation functions for the dragon
+
+
+public class DragonAnimation {
+
+	//	public static void main(String[] args) {
+	//	eggIdle();
+	//	}
+
+	//Animation for egg idling (default egg animation)
+	public static void eggIdle() {
+		//Re-add loop when we come up with better response condition
+		//while (true) {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/egg/egg_idle_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/egg/egg_idle_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		//}
+
+	}
+	//Animation for egg eating
+	public static void eggEat() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/egg/egg_eat_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/egg/egg_eat_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+	//Animation for egg being pet
+	public static void eggPet() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/egg/egg_pet_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/egg/egg_pet_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+	//Animation for baby idling (default baby animation)
+	public static void babyIdle() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/infant/baby_idle_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/infant/baby_idle_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+	//Animation for baby eating
+	public static void babyEat() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/infant/baby_eating_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/infant/baby_eating_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+	//Animation for baby being pet
+	public static void babyPet() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/infant/baby_pet_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/infant/baby_pet_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+	//Animation for adult idling (default adult animation)
+	public static void adultIdle() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/adult/adult_idle_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/adult/adult_idle_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+	//Animation for adult eating
+	public static void adultEat() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/adult/adult_eating_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/adult/adult_eating_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+
+	//Animation for adult being pet
+	public static void adultPet() {
+		StdDraw.picture(0.5,  0.5, "dragon_resources/adult/adult_pet_1.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+		StdDraw.picture(0.5, 0.5 , "dragon_resources/adult/adult_pet_2.png");
+		StdDraw.show(500);
+		StdDraw.clear();
+	}
+
+
+
+}
+
