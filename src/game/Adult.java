170f402c7eb956ed7bb77e4c74f61c8b225d6512
Katrina Ragbeer <katrinaragbeer@wustl.edu>
2020-04-05 17:31:28 -0500

    deleted unnecessary whitespace, fixed alignment issues, removed comments, added javadocs, added comments where needed

--- src/game/Adult.java
+++ src/game/Adult.java
@@ -8,67 +8,74 @@ public class Adult implements Dragon {
 	public int foodLevel;
 	public int loveLevel;
 	public int age;
-	
+
 	public Adult() {
 		this.foodLevel = 0;
 		this.loveLevel = 0;
 		this.age = 2;
 	}
-	
-	// increments the love level of the dragon if it is not already maxed out and returns the current love level
+
 	public int pet() {
 		if (this.loveLevel < 5) {
 			this.loveLevel++;
 		}
 		return this.loveLevel;
 	}
-	
-	// increments the food level of the dragon if it is not already maxed out and returns the current food level
+
 	public int feed() {
 		if (this.foodLevel < 5) {
 			this.foodLevel++;
 		}
 		return this.foodLevel;
 	}
-	
 
 	@Override
 	public void animateFeed(int frame) {
 		DragonAnimation.adultEat(frame);
 	}
-	
-	// displays the proper animation for petting the dragon
 
 	@Override
 	public void animatePet(int frame) {
 		DragonAnimation.adultPet(frame);
-		
 	}
 
 	@Override
 	public void animateIdle(int frame) {
 		DragonAnimation.adultIdle(frame);
-		
+	}
+	
+	@Override
+	public Dragon ageUp() {
+		if (this.foodLevel == 5 && this.loveLevel == 5) {
+			StdDraw.clear();
+			StdDraw.text(200, 200, "Congratulations, you raised your dragon to an adult");
+		}
+		return this;
 	}
 
+	/**
+	 * returns an interaction based on where the user clicked
+	 */
 	@Override
 	public Interactions checkInteraction(double mousex, double mousey) {
-			if ((mousex > 25 && mousex < 85) && (mousey > 20 && mousey < 100)) {
-				return Interactions.game;
-				
-			}
-			if ((mousex > 175 && mousex< 250) && (mousey > 20 && mousey<100)) {
-				 return Interactions.feed;
-			}
-			if ((mousex >350 && mousex < 410 ) && (mousey > 20 && mousey < 100)) {
-				return Interactions.pet;
-			}
-			else {
-				return Interactions.idle;
-      }
-		
-}
+		if ((mousex > 25 && mousex < 85) && (mousey > 20 && mousey < 100)) {
+			return Interactions.game;
 
+		}
+		if ((mousex > 175 && mousex< 250) && (mousey > 20 && mousey<100)) {
+			return Interactions.feed;
+		}
+		if ((mousex >350 && mousex < 410 ) && (mousey > 20 && mousey < 100)) {
+			return Interactions.pet;
+		}
+		else {
+			return Interactions.idle;
+		}
+	}
+
+	/**
+	 * displays the proper animation updates the dragon based on user interaction
+	 */
 	public void update(Interactions interactionValue, int frame) {
 		if (interactionValue ==  Interactions.idle) {
 			this.animateIdle(frame);
@@ -76,7 +83,7 @@ public class Adult implements Dragon {
 		if (interactionValue == Interactions.feed) {
 			this.animateFeed(frame);
 			if (frame == 0) {
-			this.feed();
+				this.feed();
 			}
 		}
 		if (interactionValue == Interactions.pet) {
@@ -85,16 +92,6 @@ public class Adult implements Dragon {
 				this.pet();
 			}
 		}
-		
-}
-
-	
-	@Override
-	public Dragon ageUp() {
-		if (this.foodLevel == 5 && this.loveLevel == 5) {
-		StdDraw.clear();
-		StdDraw.text(200, 200, "Congratulations, you raised your dragon to an adult");
-		}
-		return this;
 	}
+
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -5,13 +5,13 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
 
-
 	public void setUpScreen() {
 		StdDraw.setCanvasSize(400, 400);
 		StdDraw.setXscale(0,400);
 		StdDraw.setYscale(0,400);
 		StdDraw.enableDoubleBuffering();
 	}
+	
 	public void playGame() {
 		boolean clicked = false;
 		Dragon dragon = new Egg();
@@ -20,42 +20,39 @@ public class Game {
 		int frame = 0;
 		Interactions interactionLevel = Interactions.idle;
 		while (true){
-			if (frame == 30) {
+			if (frame == 30) { // resetting the frame counter each time it reaches 30 and idling the dragon
 				interactionLevel = Interactions.idle;
 				frame = 0;
 				dragon = dragon.ageUp();
 				StdDraw.clear();
 			}
 			frame++;
-			if (StdDraw.mousePressed()) {
-				 clicked = true;
-				 System.out.println("Mouse pressed");
-				 mouseX = StdDraw.mouseX();
-				 mouseY = StdDraw.mouseY();
+			if (StdDraw.mousePressed()) { // tracks the user click and stores in variables
+				clicked = true;
+				System.out.println("Mouse pressed");
+				mouseX = StdDraw.mouseX();
+				mouseY = StdDraw.mouseY();
 			}
-			if (clicked == true && !StdDraw.mousePressed()) {
-					clicked = false;
-					System.out.println("Mouse released");
-					interactionLevel = dragon.checkInteraction(mouseX, mouseY);
-					if (interactionLevel != Interactions.idle) {
-						frame = 0;
-					}
+			if (clicked == true && !StdDraw.mousePressed()) { // once the user has lifted the click, determine the interaction
+				clicked = false;
+				System.out.println("Mouse released");
+				interactionLevel = dragon.checkInteraction(mouseX, mouseY);
+				if (interactionLevel != Interactions.idle) {
+					frame = 0;
 				}
+			}
 			DragonAnimation.redrawUI();
 			dragon.update(interactionLevel,frame);
 			StdDraw.show();
 			StdDraw.pause(66);
-		
-	}
-}
-	
-	public static void main(String[] args) { 
-	Game game = new Game();
-	game.setUpScreen();
-	game.playGame();
-		
-		
 		}
-	}	
-	
+	}
+
+	public static void main(String[] args) { 
+		Game game = new Game();
+		game.setUpScreen();
+		game.playGame();
+	}
+}	
+
 
