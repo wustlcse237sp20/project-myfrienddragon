f9f42fa2c9417dec9c72d991805f277ec3afdb9c
smithj11 <julia.smith@wustl.edu>
2020-04-05 22:36:21 -0400

    Refactored game a little bit

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,14 +4,25 @@ import animations.DragonAnimation;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
+<<<<<<< Updated upstream
 
 
+=======
+	int frame;
+	boolean clicked;
+	double mouseX = 0;
+	double mouseY = 0;
+	Dragon dragon;
+	Interactions interactionLevel;
+	
+>>>>>>> Stashed changes
 	public void setUpScreen() {
 		StdDraw.setCanvasSize(400, 400);
 		StdDraw.setXscale(0,400);
 		StdDraw.setYscale(0,400);
 		StdDraw.enableDoubleBuffering();
 	}
+<<<<<<< Updated upstream
 	public void playGame() {
 		boolean clicked = false;
 		Dragon dragon = new Egg();
@@ -19,6 +30,35 @@ public class Game {
 		double mouseY = 0;
 		int frame = 0;
 		Interactions interactionLevel = Interactions.idle;
+=======
+	
+	//checks possible click interactions every frame
+	public Interactions onClick() {
+		if (StdDraw.isMousePressed()) {
+			 clicked = true;
+			 mouseX = StdDraw.mouseX();
+			 mouseY = StdDraw.mouseY();
+		}
+		if (clicked == true && !StdDraw.isMousePressed()) {
+				clicked = false;
+				 interactionLevel = dragon.checkInteraction(mouseX, mouseY);
+				if (interactionLevel != Interactions.idle) {
+					frame = 0;
+				}
+			}
+		return interactionLevel;
+	}
+	
+	public boolean isMousePressed() {
+		return StdDraw.isMousePressed();
+	}
+	
+	//updates frames, checks interactions
+	public void playGame() {
+		clicked = false;
+		dragon = new Egg();
+		interactionLevel = Interactions.idle;
+>>>>>>> Stashed changes
 		while (true){
 			if (frame == 30) {
 				interactionLevel = Interactions.idle;
@@ -27,6 +67,7 @@ public class Game {
 				StdDraw.clear();
 			}
 			frame++;
+<<<<<<< Updated upstream
 			if (StdDraw.mousePressed()) {
 				 clicked = true;
 				 System.out.println("Mouse pressed");
@@ -41,6 +82,10 @@ public class Game {
 						frame = 0;
 					}
 				}
+=======
+			System.out.println(frame);
+			interactionLevel = this.onClick();
+>>>>>>> Stashed changes
 			DragonAnimation.redrawUI();
 			dragon.update(interactionLevel,frame);
 			StdDraw.show();
