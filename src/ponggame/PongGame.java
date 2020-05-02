9bc01d8ca600a98c96c88790aa58e1f2f48d27a0
smithj11 <julia.smith@wustl.edu>
2020-05-02 15:05:53 -0400

    Game now tracks score, player can choose between play with ai and play with another person

--- src/ponggame/PongGame.java
+++ src/ponggame/PongGame.java
@@ -5,9 +5,35 @@ import java.awt.event.KeyEvent;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class PongGame {
-	RealPlayer player;
-	ProjectPlayer AIplayer;
+	RealPlayer playerOne;
+	Player playerTwo;
 	Ball ball;
+	
+public void drawSetupMessage() {
+	StdDraw.clear();
+	StdDraw.text(200, 200, "Press 1 for single-player pong");
+	StdDraw.text(200, 180, "Press 2 for classic two-player pong");
+	StdDraw.show();
+}
+	
+public void setUpOneorTwoPlayer() {
+	this.drawSetupMessage();
+	boolean goodInput = false;
+	while (!goodInput) {
+
+		this.drawSetupMessage();
+
+		if (StdDraw.isKeyPressed(KeyEvent.VK_2)) {
+			playerTwo = new RealPlayer(350,200, 2);
+			goodInput = true;
+		}
+		else if (StdDraw.isKeyPressed(KeyEvent.VK_1)) {
+			playerTwo = new ProjectPlayer();
+			goodInput = true;
+		}
+	}
+	
+}
 	
 public void setUpGameScreen() {
 	StdDraw.setCanvasSize(400, 400);
@@ -16,21 +42,34 @@ public void setUpGameScreen() {
 	StdDraw.enableDoubleBuffering();
 }
 public void drawPlayerPaddle() {
-	StdDraw.filledRectangle(player.getxPos(), player.getyPos(), player.getWidth(), player.getHeight());
+	StdDraw.filledRectangle(playerOne.getxPos(), playerOne.getyPos(), playerOne.getWidth(), playerOne.getHeight());
 	
 }
 public void drawAIPaddle() {
-	StdDraw.filledRectangle(AIplayer.getxPos(), AIplayer.getyPos(), AIplayer.getWidth(), AIplayer.getHeight());
+	StdDraw.filledRectangle(playerTwo.getxPos(), playerTwo.getyPos(), playerTwo.getWidth(), playerTwo.getHeight());
 	
 }
 public void drawBall() {
 	StdDraw.filledCircle(ball.getxPos(), ball.getyPos(), ball.radius);
 }
 
+public void drawScore() {
+	StdDraw.rectangle(50, 0, 50, 15);
+	StdDraw.rectangle(300, 0, 50, 15);
+	StdDraw.text(50, 5, "Score: " + playerOne.getScore());
+	StdDraw.text(300, 5,"Score: " + playerTwo.getScore());
+}
+
 public void update() {
-	this.takeMovement();
-	ball.update(player, AIplayer);
-	AIplayer.update(ball, player);
+	Collisions collision = ball.update(playerOne, playerTwo);
+	playerOne.update(collision);
+	if (playerTwo instanceof ProjectPlayer) {
+	((ProjectPlayer) playerTwo).update(ball, playerOne, collision);
+	}
+	else {
+		((RealPlayer) playerTwo).update(collision);
+	}
+	System.out.println("Player two score:: " +playerTwo.getScore());
 }
 
 public void drawGame() {
@@ -38,25 +77,14 @@ public void drawGame() {
 	this.drawPlayerPaddle();
 	this.drawBall();
 	this.drawAIPaddle();
+	this.drawScore();
 	StdDraw.show();
-	StdDraw.pause(66);
+	StdDraw.pause(33);
 	
 }
-public void takeMovement() {
-	if (StdDraw.isKeyPressed(KeyEvent.VK_W) || StdDraw.isKeyPressed(KeyEvent.VK_S)) {
-		if (StdDraw.isKeyPressed(KeyEvent.VK_W)) {
-			player.moveUp();	
-		}
-		if (StdDraw.isKeyPressed(KeyEvent.VK_S)) {
-			player.moveDown();
-		}
-	}
-	
-	
-}
+
 public void playGame() {
-	player = new RealPlayer();
-	AIplayer = new ProjectPlayer();
+	playerOne = new RealPlayer(50, 200, 1);
 	ball = new Ball();
 	while (true) {
 		this.update();
@@ -67,6 +95,7 @@ public void playGame() {
 public static void main (String[]args) {
 	PongGame game = new PongGame();
 	game.setUpGameScreen();
+	game.setUpOneorTwoPlayer();
 	game.playGame();
 	
 }
