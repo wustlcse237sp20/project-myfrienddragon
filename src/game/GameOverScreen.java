cd5beb8cbc017309b5d6a5571f270f06dfeff423
Julia Smith <42749098+smithj11@users.noreply.github.com>
2020-05-04 05:19:37 -0400

    Fixed the same merge conflict AGAIN.

--- src/game/GameOverScreen.java
+++ src/game/GameOverScreen.java
@@ -8,95 +8,74 @@ import game_abstractions.GameManager;
 import game_abstractions.GameScene;
 import ponggame.PongGame;
 
-<<<<<<< HEAD
 public class GameOverScreen extends GameScene{
-		double mouseX;
-		double mouseY;
-		boolean clicked;
-		GameMenuInteractions interactionLevel;
-		int frame;
-public GameOverScreen(GameManager manager, GameScene parent) {
-			super(manager, parent);
-			this.mouseX=0;
-			this.mouseY=0;
+	double mouseX;
+	double mouseY;
+	boolean clicked;
+	GameMenuInteractions interactionLevel;
+	int frame;
+	public GameOverScreen(GameManager manager, GameScene parent) {
+		super(manager, parent);
+		this.mouseX=0;
+		this.mouseY=0;
+		clicked = false;
+		frame = 0;
+	}
+
+	public void drawGameMenu() {
+		UIAnimations.drawGameOverScreen();
+	}
+
+	public GameMenuInteractions renewInteraction() {
+		if (StdDraw.isMousePressed()) {
+			clicked = true;
+			mouseX = StdDraw.mouseX();
+			mouseY = StdDraw.mouseY();
+		}
+		if (clicked == true && !StdDraw.isMousePressed()) {
 			clicked = false;
-			frame = 0;
-		}
-
-		public void drawGameMenu() {
-			UIAnimations.drawGameOverScreen();
-		}
-		
-		public GameMenuInteractions renewInteraction() {
-			if (StdDraw.isMousePressed()) {
-				 clicked = true;
-				 mouseX = StdDraw.mouseX();
-				 mouseY = StdDraw.mouseY();
-			}
-			if (clicked == true && !StdDraw.isMousePressed()) {
-					clicked = false;
-					 interactionLevel = this.checkSceneInteraction(mouseX, mouseY);
-					if (interactionLevel != GameMenuInteractions.wait) {
-						frame = 0;
-					}
-					
-				}
-			System.out.println("Interaction level: " + interactionLevel);
-			return interactionLevel;
-		}
-
-		public GameMenuInteractions checkSceneInteraction(double mouseX, double mouseY) {
-			StdDraw.filledRectangle(200,50,100,50);
-			if ((mouseX > 100 && mouseX < 300) && (mouseY > 0 && mouseY < 100)) {
-				return GameMenuInteractions.restart;
-			}
-			return GameMenuInteractions.wait;
-		}
-
-		
-		public void respondToInteraction(GameMenuInteractions interactionLevel, int frame) {
-			
-			if (interactionLevel == GameMenuInteractions.restart) {
-				GameScene game = new Game(this.getGameManager(), null);
-				this.getGameManager().setScene(game);
-			}
-			
-			if (interactionLevel == GameMenuInteractions.wait) {
-				this.drawGameMenu();
-			}
-			
-			
-		}
-		public void update() {
-			if (frame == 30) {
-				interactionLevel = GameMenuInteractions.wait;
+			interactionLevel = this.checkSceneInteraction(mouseX, mouseY);
+			if (interactionLevel != GameMenuInteractions.wait) {
 				frame = 0;
-				StdDraw.clear();
 			}
-			frame++;
-			System.out.println(frame);
-			interactionLevel = this.renewInteraction();
-			this.respondToInteraction(interactionLevel, frame);
-			}
+
 		}
-=======
-public class GameOverScreen implements GameEntity{
-	
-	public void drawGameOverScreen() {
-		UIAnimations.drawGameOverScreen();	
+		System.out.println("Interaction level: " + interactionLevel);
+		return interactionLevel;
 	}
 
-	@Override
-	public void update(Interactions interactionLevel, int frame) {
-		this.drawGameOverScreen();
+	public GameMenuInteractions checkSceneInteraction(double mouseX, double mouseY) {
+		StdDraw.filledRectangle(200,50,100,50);
+		if ((mouseX > 100 && mouseX < 300) && (mouseY > 0 && mouseY < 100)) {
+			return GameMenuInteractions.restart;
+		}
+		return GameMenuInteractions.wait;
 	}
 
-	@Override
-	public Interactions checkInteraction(double mouseX, double mouseY) {
-		if ((mouseX > 150 && mouseX < 250) && (mouseY> 0 && mouseY <100)) {
-			return Interactions.restart;
+
+	public void respondToInteraction(GameMenuInteractions interactionLevel, int frame) {
+
+		if (interactionLevel == GameMenuInteractions.restart) {
+			GameScene game = new Game(this.getGameManager(), null);
+			this.getGameManager().setScene(game);
 		}
-		return Interactions.wait;
+
+		if (interactionLevel == GameMenuInteractions.wait) {
+			this.drawGameMenu();
+		}
+
+
 	}
->>>>>>> develop
+	public void update() {
+		if (frame == 30) {
+			interactionLevel = GameMenuInteractions.wait;
+			frame = 0;
+			StdDraw.clear();
+		}
+		frame++;
+		System.out.println(frame);
+		interactionLevel = this.renewInteraction();
+		this.respondToInteraction(interactionLevel, frame);
+	}
+}
 
